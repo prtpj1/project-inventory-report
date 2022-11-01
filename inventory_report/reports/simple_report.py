02ece48257a5ac2338ad0dcf2a331ce4c49db8a4
prtpj1 <prtpj1@gmail.com>
2022-10-31 22:47:39 -0300

    :nerd_face: refactor:(Requisite 03) - change variables into methods :arrow_right: to use that code into CompleteReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,19 +3,33 @@ from collections import Counter
 
 
 class SimpleReport:
-    @classmethod
-    def generate(self, list):
+    # @classmethod
+    def oldest_date(list):
+        date = min(product["data_de_fabricacao"] for product in list)
+        return date
+
+    # @classmethod
+    def closest_date(list):
         TODAY = datetime.today().isoformat()
-        oldest_date = min(product["data_de_fabricacao"] for product in list)
-
-        closest_date = min(
+        date = min(
             product["data_de_validade"]
-            for product in list if product["data_de_validade"] >= TODAY
+            for product in list
+            if product["data_de_validade"] >= TODAY
         )
+        return date
 
+    # @classmethod
+    def company_with_bigger_stock(list):
         company = Counter(
             [company_name["nome_da_empresa"] for company_name in list]
         ).most_common()[0][0]
+        return company
+
+    @classmethod
+    def generate(cls, list):
+        oldest_date = cls.oldest_date(list)
+        closest_date = cls.closest_date(list)
+        company = cls.company_with_bigger_stock(list)
 
         return (
             f"Data de fabricação mais antiga: {oldest_date}\n"
